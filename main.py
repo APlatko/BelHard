cd9b5661e95166c7a631db714ec88f31b3b6e1e7
APlatko <sale.itvendor@gmail.com>
2022-04-24 12:39:57 +0300

    Homework 3

--- main.py
+++ main.py
@@ -1,16 +1,28 @@
-# This is a sample Python script.
+a, b, c, d, e = 5, 7, 10, 370, 20
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+result = a + b
+print(result)
+result /= c
+print(result)
+print(type(result))
+print(round(result**2 * 10), "\n")
+
+print(result)
+result = int(result)
+print(result, type(result))
+print(result * d)
+print(d // e)
+print(d % e)
+print("")
+
+print(d > c and d < e)
+vl = bool(print(c > b or c >= e))
+print(type(vl))
+print(vl == 0, vl != 0)
+
+print("")
+phrase = f"London is the capital with {c} mil. citizens."
+print(type(phrase), phrase)
+print("10" in phrase)
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
