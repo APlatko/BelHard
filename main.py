a3b71140e0f01aa0526519b6441c5303c07f79cd
APlatko <sale.itvendor@gmail.com>
2022-05-15 13:39:48 +0300

    Lesson 8

--- main.py
+++ main.py
@@ -1,16 +1,96 @@
-# This is a sample Python script.
+class Animal:
+    name: str
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+    def __init__(self, name):
+        self.name = name
+
+    def says(self):
+        return self
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+class Cat(Animal):
+
+    def says(self):
+        return f"{self.name} - a cat. It says MEOW."
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+class Dof(Animal):
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    def says(self):
+        return f"{self.name} - a dog. It says WOOF."
+
+
+class Cow(Animal):
+
+    def says(self):
+        return f"{self.name} - a cow. It says MUU."
+
+
+cat_1 = Cat("Sindy")
+dog_1 = Dof("Sparky")
+cow_1 = Cow("Dosya")
+a = Animal("s")
+
+print(cat_1.says())
+print(dog_1.says())
+print(cow_1.says())
+
+
+# _______________________________________
+
+class Shape:
+
+    def get_perimeter(self):
+        return self
+
+    def get_square(self):
+        return self
+
+
+class Circle(Shape):
+    r: float
+
+    def __init__(self, r):
+        self.radius = r
+
+    def get_perimeter(self):
+        perimeter = 2 * 3.14 * self.radius
+        return perimeter
+
+    def get_square(self):
+        square = 3.14 * self.radius ** 2
+        return square
+
+
+class Rectangle(Shape):
+    a: float
+    b: float
+
+    def __init__(self, a, b):
+        self.side_a = a
+        self.side_b = b
+
+    def get_perimeter(self):
+        perimeter = 2 * (self.side_a + self.side_b)
+        return perimeter
+
+    def get_square(self):
+        square = self.side_a * self.side_b
+        return square
+
+
+class Square(Rectangle):
+
+    def __init__(self, a):
+        super().__init__(a, a)
+
+
+a = Circle(3)
+b = Rectangle(2, 2)
+c = Square(5)
+print(a.get_perimeter())
+print(a.get_square())
+print(b.get_perimeter())
+print(b.get_square())
+print(c.get_perimeter())
+print(c.get_square())
